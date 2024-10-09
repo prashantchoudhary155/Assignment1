682333e9a3bd23ddb2c989b8a44e740214c2a1c7
prashant <choudharyprashant155@gmail.com>
2024-10-09 20:56:22 +0530

    fix:vercel deployment

--- api/index.js
+++ api/index.js
@@ -104,3 +104,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
